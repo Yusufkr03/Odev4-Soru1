41e4ac751d16dba037f1257a9b5f3ea08e759f8c
Yusufkr03 <yusufkr2903@gmail.com>
2024-11-21 11:38:29 +0300

    Add files via upload

---
+++ Odev4-Soru1/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics.Metrics;
+using System.Reflection.Metadata;
+using System;
+
+
+namespace odev
+{
+    class metod
+    {
+        public static double alan()
+        {
+            double taban, yukseklik, alan;
+            
+            Console.WriteLine("Taban uzunluğunu giriniz");
+            taban = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Yüksekliği giriniz");
+            yukseklik = Convert.ToDouble(Console.ReadLine());
+            alan = taban * yukseklik / 2;
+            Console.WriteLine("--Üçgenin Alanı--");
+
+
+            return alan;
+
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(metod.alan());
+
+
+        }
+
+
+
+
+    }
+
+
+
+}
+
